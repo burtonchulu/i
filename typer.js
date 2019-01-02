8daa96039d15f639fb3b7ee8070386c9254c747e
burtonchulu <burtchulu84@gmail.com>
2019-01-02 16:40:55 +0200

    Add files via upload

---
+++ typer.js
@@ -0,0 +1,50 @@
+var i = 0;
+var j = 0;
+
+var text = "Welcome to my portfolio.My name is Burton Chulu. I started learning web development in 2015. Along the way i have picked up numerous skills. I can code HTML5 CSS3 JavaScript Ajax PHP MySQL and do  Responsive design as well as mobile first approach.";
+
+var text2 = "Below are some of my projects that i have worked on :";
+
+function callTypeWriter(){
+
+document.getElementById("pi").innerHTML  = "";
+document.getElementById("pi2").innerHTML  = "";
+
+if(document.getElementById("pi").innerHTML==""){
+
+typewriter();
+
+}
+}
+
+
+function typewriter(){
+
+if(i<text.length){
+
+document.getElementById("pi").innerHTML  += text.charAt(i).toUpperCase();
+i++;
+auto = setTimeout(typewriter,50);
+
+}
+
+if(i==text.length){
+
+typewriter2();
+
+}
+
+}
+
+function typewriter2(){
+
+if(j<text2.length){
+
+document.getElementById("pi2").innerHTML  += text2.charAt(j).toUpperCase();
+j++;
+
+setTimeout(typewriter2,50);
+
+}
+
+}
