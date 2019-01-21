b8b7747416aeaec8dbf28f00ef2697981845201e
burtonchulu <burtchulu84@gmail.com>
2019-01-21 07:16:50 +0200

    Add files via upload

---
+++ word.js
@@ -0,0 +1,124 @@
+window.onload = wordArray;
+
+var words = [];
+
+var wordbank = [];
+
+function wordArray(){
+
+var request = new XMLHttpRequest();
+
+request.open("GET","https://api.datamuse.com/words?sp=??????");
+
+request.onreadystatechange = function(){
+
+if(this.readyState==this.DONE && this.status==200){
+
+parseWords(this.responseText);
+
+displayScrambledWord();
+
+}
+
+};
+
+request.send();
+
+}
+
+function parseWords(jsonWords){
+
+if(!jsonWords){
+
+return;
+
+}
+
+var wordObjects = JSON.parse(jsonWords);
+
+for(var i=0;i<wordObjects.length;++i){
+
+var word = wordObjects[i].word;
+
+words.push(word);
+
+}
+
+}
+
+function displayScrambledWord(){
+
+reset();
+
+var len = words.length;
+
+var word = [];
+
+var rand = Math.floor(Math.random()*len);
+
+var disp = document.getElementById("disp");
+
+var a = words[rand];
+
+wordbank.push(a);
+
+for(var i=0;i<a.length;++i){
+
+var letter = a.charAt(i);
+
+word.push(letter);
+
+}
+
+var scrambledword = word.sort().join('');
+
+disp.innerHTML = scrambledword;
+
+}
+
+function enterAnswer(){
+
+disp2.innerHTML = document.getElementById("ans").value;
+
+if(disp2.innerHTML==wordbank[0]){
+
+alert("Correct");
+
+reset();
+
+setTimeout(displayScrambledWord,500);
+
+}
+else{
+
+alert("Incorrect");
+
+resetInputField(ans);
+
+}
+
+}
+
+function reset(){
+
+wordbank = [];
+
+disp.innerHTML = "";
+
+disp2.innerHTML = "";
+
+resetInputField(ans);
+
+}
+
+function showAnswer(){
+
+disp2.innerHTML = wordbank[0];
+
+}
+
+function resetInputField(arg){
+
+arg.value = "";
+
+}
